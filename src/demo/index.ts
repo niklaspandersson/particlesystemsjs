853732ce8f3ac1bee424ed8ea928386443e3fa32
Niklas Andersson <niklas.andersson2@vaxjo.se>
2020-08-30 08:39:15 +0200

    emitting random particles. support for sprites

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -1,23 +1,39 @@
-import {ParticleSystem, random, randomize, createFactoryFromVector } from "../index";
+import {ParticleSystem, random, randomize, createFactoryFromVector, Particle } from "../index";
+import { Sprites } from "./Sprites";
 
 const scene = document.getElementById("scene") as HTMLCanvasElement;
-const Width = 12;
-const Height = 5;
+const sprites = new Sprites("./sprites.png");
 
+const Size = 2;
+const Width = 12*Size;
+const Height = 6*Size;
+
+const halfWidth = Width/2;
+const halfHeight = Height/2;
+
+type ParticleUserData = {
+  initialRotation: number,
+  rotationSpeed: number,
+  color: string
+}
 scene.addEventListener('click', ev => {
-  const test = new ParticleSystem({ 
-    count: 180,
-    dampening: .99,
-    position: {x: scene.width/2, y: scene.height/2},
+  const test = new ParticleSystem<ParticleUserData>({ 
+    initialCount: 0,
+    position: {x: ev.x, y: ev.y},
     forces: {
-      gravity: {x: 0, y: 98},
-      wind: { x: 20, y: 0}
+      gravity: {x: 0, y: 298},
+      wind: { x: 60, y: 0}
+    },
+    emitter: {
+      particlesPerSecond: 60,
+      strategy: "random"
     },
     onInit: init,
     onUpdate: draw,
     particle: {
+      //initialPos: {x: {min: -50, max: 50}, y: {min: -50, max: 50}},
       initialPos: {x: 0, y: 0},
-      initialVelocity: createFactoryFromVector({x: -230, y: -120}, Math.PI/4, 0.3), //{ x: {min: -50,max: 50}, y: {min: -50, max: 50}},
+      initialVelocity: createFactoryFromVector({x: 0, y: -400}, Math.PI/5, .6),
       lifetime: {min: .5, max: 4}
     }
   });
@@ -27,35 +43,30 @@ scene.addEventListener('click', ev => {
 
 
 
-function init(ps:ParticleSystem) {
-  for(const p of ps.particles) 
-    p.userData = {
-      initialRotation: random({min:0, max: Math.PI*2}),
-      rotationDirection: randomize([-1, 1]),
-      rotationSpeed: random({min: .3, max: 2}),
-      color: randomize(['orange', 'lightblue', 'green', 'red'])
-    };
+function init(p:Particle<ParticleUserData>) {
+  p.userData = {
+    initialRotation: random({min:0, max: Math.PI*2}) * randomize([-1, 1]),
+    rotationSpeed: random({min: .2, max: 2}),
+    color: randomize(['orange', 'lightblue', 'green', 'red'])
+  };
 }
 
-let ctx = scene.getContext("2d")!;
-const halfWidth = Width/2;
-const halfHeight = Height/2;
-
-function draw(ps:ParticleSystem) {
+function draw(ps:ParticleSystem<ParticleUserData>, dt:number) {
   ///DRAWING
+  let ctx = scene.getContext("2d")!;
+
   ctx.clearRect(0,0,scene.width, scene.height);
   let lastColor = "";
   ps.particles.forEach(function(p) {
-    if(p.userData.color !== lastColor) {
-      ctx.fillStyle = p.userData.color;
-      lastColor = p.userData.color;
-    }
+    const userData = p.userData!;
 
     ctx.save();
     ctx.globalAlpha = 1-p.normalizedAge;
+    ctx.fillStyle = p.userData?.color!;
     ctx.translate(ps.pos.x + p.pos.x, ps.pos.y + p.pos.y);
-    ctx.rotate(p.userData.initialRotation + Math.PI*2*p.normalizedAge**2*p.userData.rotationSpeed*p.userData.rotationDirection)
+    ctx.rotate(userData.initialRotation + Math.PI*2*p.normalizedAge**2*userData.rotationSpeed)
     ctx.fillRect(-halfWidth, -halfHeight, halfWidth, halfHeight);
+    //sprites.drawSprite(ctx, "Ring", 0, 0, p.normalizedAge);
     ctx.restore();
   });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
+import gaussian from "./math/gaussian";
+
 type Vec2d<T> = {
   x:T;
   y:T;
 }
 
 function magnitude(value:Vec2d<number>) {
-  return Math.sqrt(value.x**2 + value.y**2)
+  return Math.sqrt(value.x*value.x + value.y*value.y)
 }
 function normalize(value:Vec2d<number>) {
   const m = magnitude(value);
   return { x: value.x/m, y: value.y/m }
 }
 
-
-type Color = string | { r: number, g: number, b: number } | number;
-
-class Vec2
+class Vec2 implements Vec2d<number>
 {
   private data:number[] = [0, 0];
   constructor({x = 0, y = 0} = {}) {
@@ -53,17 +52,18 @@ class Entity
   }
 }
 
-class Particle extends Entity
+export class Particle<T> extends Entity
 {
   private lifetime:number;
   public age:number;
   public normalizedAge:number;
-  public userData:any;
+  public userData:T|null;
 
-  constructor(options:ParticleOptions&ParticleFactories) {
+  constructor(options:ParticleOptions<T>&ParticleFactories) {
     super(new Vec2(options.createPosition()), new Vec2(options.createVelocity()));
     this.lifetime = options.createLifetime();
     this.age = this.normalizedAge = 0;
+    this.userData = null;
   }
 
   update(forces:Vec2d<number>, dampening:number, dt:number) {
@@ -88,39 +88,36 @@ type ParticleFactories = {
   createLifetime: () => number
 };
 
-type ParticleOptions = { 
+
+type ParticleEmitterOptions = {
+  particlesPerSecond: number;
+  strategy: "random" | "periodical";
+  lifetime?: number;
+}
+
+type ParticleOptions<T> = { 
   initialPos: Vec2d<number|NumRange>|(() => Vec2d<number>); 
   initialVelocity: Vec2d<number|NumRange>|(() => Vec2d<number>);
   lifetime?: number|NumRange|(() => number);
+  compareFn?: (p1:Particle<T>, p2:Particle<T>)=>number;
 };
 
 type Vec2Dictionary = { [key:string]: Vec2d<number> };
-type ParticleSystemOptions = {
-  count: number;
+type ParticleSystemOptions<T> = {
+  initialCount: number;
   position: Vec2d<number>;
-  particle: ParticleOptions;
-  forces:  Vec2Dictionary,
-  dampening?: number,
-  onInit?: (ps:ParticleSystem) => void,
-  onUpdate?: (ps:ParticleSystem, dt:number) => void
+  forces:  Vec2Dictionary;
+  dampening?: number;
+  particle: ParticleOptions<T>;
+  emitter?: ParticleEmitterOptions;
+  onInit?: (ps:Particle<T>) => void;
+  onUpdate?: (ps:ParticleSystem<T>, dt:number) => void;
 }
 
 const DefaultParticleLifetime = 3;
-const DefaultPSOptions:ParticleSystemOptions = {
-  count: 20,
-  position: { x: 0, y: 0},
-  forces: {
-    //gravity: { x: 0, y: -9.8 }
-  },
-  dampening: 1,
-  particle: {
-    initialPos: { x: {min: -10, max: 10}, y: 0 },
-    initialVelocity: { x: 0, y: {max: -10, min: -100} }
-  }
-}
 
 export function random({min, max}:NumRange) { return min + Math.random()*(max-min) };
-export function randomize(values:any[]) { return values[Math.floor(Math.random()*values.length)] };
+export function randomize<T>(values:T[]) { return values[Math.floor(Math.random()*values.length)] };
 
 function createNumberFactory(param:number|NumRange) {
   if(typeof param === 'number') {
@@ -131,18 +128,24 @@ function createNumberFactory(param:number|NumRange) {
   }    
 }
 
-export function createFactoryFromVector(vec:Vec2d<number>, spread:number, speedFactor:number) {
+/**
+ * 
+ * @param vec A vector describing the general direction and speed of the particles
+ * @param spread An angle in radians that define the spread of particles, centered at the vector `vec`
+ * @param minSpeedFactor A factor that defines the lowest speed in relation to the vector `vec`
+ */
+export function createFactoryFromVector(vec:Vec2d<number>, spread:number = 0, minSpeedFactor:number = 1) {
   const mag = magnitude(vec);
   const dir = normalize(vec);
 
-  let angle = dir.x !== 0 ? Math.atan(dir.y / dir.x) : 90;
+  let angle = dir.x !== 0 ? Math.atan(dir.y / dir.x) : Math.PI/2;
   if(dir.y < 0)
     angle += Math.PI;
 
   const minAngle = angle - spread/2;
   const maxAngle = angle + spread/2;
 
-  const minMag = mag*speedFactor;
+  const minMag = mag*minSpeedFactor;
 
   return function() {
     const m = random({min: minMag, max: mag});
@@ -151,35 +154,79 @@ export function createFactoryFromVector(vec:Vec2d<number>, spread:number, speedF
   }
 }
 
-function createValueFactory(key:"x"|"y", vec:Vec2d<number|NumRange>) {
-  return createNumberFactory(vec[key]);  
-}
-
 function createVec2dFactory(src:Vec2d<number|NumRange>):()=>Vec2d<number> {
-  const xFactory = createValueFactory("x", src);
-  const yFactory = createValueFactory("y", src);
+  const xFactory = createNumberFactory(src.x);
+  const yFactory = createNumberFactory(src.y);
 
-  return function() {
-    return { x: xFactory(), y: yFactory() };
-  }
+  return () => ({ x: xFactory(), y: yFactory() });
 };
 
-export class ParticleSystem
+
+class ParticleEmitter<T> {
+  private _opts: ParticleEmitterOptions;
+  private _particleOpts:ParticleOptions<T>&ParticleFactories|null;
+  constructor(opts:ParticleEmitterOptions) {
+    this._opts = opts;
+    this._particleOpts = null;
+  }
+
+  onInit(initialCount:number, particleOpts:ParticleOptions<T>&ParticleFactories) {
+    this._particleOpts = particleOpts;
+    return Array(initialCount).fill(null).map(_ => new Particle(particleOpts));
+  }
+  onUpdate(dt:number) {
+    const cnt = this.randomParticleCount(dt);
+    return cnt ? Array(cnt).fill(null).map(_ => new Particle(this._particleOpts!)) : null;
+  }
+
+  private randomParticleCount(dt:number) {
+    const fracMean = this._opts.particlesPerSecond*dt;
+    let instant = 0;
+    do {
+      instant = gaussian(fracMean, .8)
+    } while(Math.abs(fracMean-instant) > fracMean)
+
+    return Math.round(instant);
+  }
+}
+
+export class ParticleSystem<T>
 {
-  private _particles:Particle[];
+  private _emitter:ParticleEmitter<T>;
+  private _particles:Particle<T>[];
   public get particles() { return this._particles; }
 
   private _pos:Vec2d<number>;
   public get pos() { return this._pos; }
 
-  private onUpdate: (ps:ParticleSystem, dt:number) => void;
-  private onInit: (ps:ParticleSystem) => void;
+  private onUpdate: (ps:ParticleSystem<T>, dt:number) => void;
+  private onInit: (ps:Particle<T>) => void;
 
   private forces:Vec2Dictionary;
   private dampening:number;
 
-  constructor(options:Partial<ParticleSystemOptions>) {
-    const opts = {...DefaultPSOptions, ...options};
+  constructor(options:Partial<ParticleSystemOptions<T>>) {
+    const DefaultPSOptions:ParticleSystemOptions<T> = {
+      initialCount: 20,
+      position: { x: 0, y: 0},
+      forces: {
+        //gravity: { x: 0, y: -9.8 }
+      },
+      dampening: 1,
+      particle: {
+        initialPos: { x: {min: -10, max: 10}, y: 0 },
+        initialVelocity: { x: 0, y: {max: -10, min: -100} }
+      }
+    }
+
+    const DefaultEmitterOptions:ParticleEmitterOptions = {
+      particlesPerSecond: 20,
+      strategy: "random",
+      lifetime: Infinity
+    }
+
+    const opts = {...DefaultPSOptions,...options};
+
     this.forces = opts.forces;
     this._pos = opts.position;
     this.dampening = opts.dampening || 1;
@@ -187,48 +234,80 @@ export class ParticleSystem
     this.onInit = opts.onInit || (() => {});
 
     const factories:ParticleFactories = {
-      createPosition: (typeof opts.particle.initialPos === 'function') ? opts.particle.initialPos : createVec2dFactory(opts.particle.initialPos),
-      createVelocity: (typeof opts.particle.initialVelocity === 'function') ? opts.particle.initialVelocity : createVec2dFactory(opts.particle.initialVelocity),
-      createLifetime: (typeof opts.particle.lifetime === 'function') ? opts.particle.lifetime : createNumberFactory(opts.particle.lifetime || DefaultParticleLifetime)
+      createPosition: (typeof opts.particle.initialPos === 'function') 
+        ? opts.particle.initialPos 
+        : createVec2dFactory(opts.particle.initialPos),
+
+      createVelocity: (typeof opts.particle.initialVelocity === 'function') 
+        ? opts.particle.initialVelocity 
+        : createVec2dFactory(opts.particle.initialVelocity),
+
+      createLifetime: (typeof opts.particle.lifetime === 'function') 
+        ? opts.particle.lifetime 
+        : createNumberFactory(opts.particle.lifetime || DefaultParticleLifetime)
     }
     const particleOptions = {...opts.particle, ...factories};
-    
-    this._particles = Array(opts.count).fill(null).map(_ => new Particle(particleOptions));
 
+    this._emitter = new ParticleEmitter<T>({...DefaultEmitterOptions, ...opts.emitter});
+    
+    this._particles = this._emitter.onInit(opts.initialCount, particleOptions);
+    for(const p of this._particles) 
+      this.onInit(p);
+
+    if(opts.particle?.compareFn)
+      this._particles = this.particles.sort(opts.particle.compareFn);
+
+    this.doStart = this.doStart.bind(this); 
     this.doUpdate = this.doUpdate.bind(this);
   }
 
   private lastTime: DOMHighResTimeStamp = 0;
-  private onStart = (time:DOMHighResTimeStamp) => {
-    this.lastTime = time;
-    this.onInit(this);
-    this._particles = this.particles.sort((p1,p2) => p1.userData.color.localeCompare(p2.userData.color));
-    window.requestAnimationFrame(this.doUpdate);
-  }
+  private animationFrameId: number|null = null;
 
   public start() {
-    window.requestAnimationFrame(this.onStart);
+    if(!this.animationFrameId)
+      this.animationFrameId = window.requestAnimationFrame(this.doStart);
+  }
+  public stop() {
+    if(this.animationFrameId) 
+      window.cancelAnimationFrame(this.animationFrameId);
+    this.animationFrameId = null;
+  }
+
+  private doStart(time:DOMHighResTimeStamp) {
+    this.lastTime = time;
+    this.animationFrameId = window.requestAnimationFrame(this.doUpdate);
   }
 
   private doUpdate(time:DOMHighResTimeStamp) {
     const dt = (time - this.lastTime) / 1000;
     this.lastTime = time;
+
+    const newParticles = this._emitter.onUpdate(dt);
+    if(newParticles) {
+      for(const p of newParticles) 
+        this.onInit(p);      
+        
+      this.particles.push(...newParticles);
+    }
   
     this.update(dt);
+
     this.onUpdate(this, dt);
-    window.requestAnimationFrame(this.doUpdate);
+    this.animationFrameId = window.requestAnimationFrame(this.doUpdate);
   }
 
-  public update(dt:number) {
-    const dampening = this.dampening;
+  private update(deltaTime:number) {
     let forces = new Vec2();
     for(const f in this.forces)
       forces.add(this.forces[f]);
 
-    const up = function(p:Particle) {
-      return p.update(forces, dampening, dt);
-    }
+    const ctx = { forces, dampening: this.dampening, deltaTime };
 
-    this._particles = this._particles.filter(up);
+    this._particles = this._particles.filter(up, ctx);
   }
+}
+
+function up<T>(this:{forces:Vec2, dampening:number, deltaTime:number}, p:Particle<T>) {
+  return p.update(this.forces, this.dampening, this.deltaTime);
 }
